30e9639bea5b8f0b42a579f4a0c2c29f3861623a
nikitozeg <72621010a>
2015-05-29 12:37:51 +0300

    Energia price crowler by API

---
+++ src/qa/vozovoz/ru/requestEnergia.java
@@ -0,0 +1,78 @@
+package qa.vozovoz.ru;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+public class requestEnergia {
+
+    private final String USER_AGENT = "Mozilla/5.0";
+
+    public static void main(String[] args) throws Exception {
+
+        requestEnergia http = new requestEnergia();
+
+        System.out.println("Testing 1 - Send Http GET request");
+        http.sendGet();
+
+
+    }
+
+    // HTTP GET request
+    private void sendGet() throws Exception {
+
+        String url = "http://api.nrg-tk.ru/api/rest/?callback=jsonp1236078926969&method=nrg.calculate&from=8362&to=495&weight=1&volume=0&place=1";
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        //add request header
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+
+        //print result
+        System.out.println(response.toString());
+
+        String json = "{\"paramsArray\": [\"first\", 100], "
+                + "\"paramsObj\": {\"one\": \"two\", \"three\": \"four\"}, "
+                + "\"paramsStr\": \"some string\"}";
+
+
+        System.out.println(json);
+        JSONParser parser = new JSONParser();
+
+        Object objj = parser.parse(json);
+        JSONObject jsonObj = (JSONObject) objj;
+        System.out.println(jsonObj.get("paramsStr"));
+// some string
+
+        JSONObject jo = (JSONObject) jsonObj.get("paramsObj");
+        System.out.println(jo.get("three"));
+// four
+
+        JSONArray ja = (JSONArray) jsonObj.get("paramsArray");
+        System.out.println(ja.get(1));
+// 100
+    }
+
+
+
+}
